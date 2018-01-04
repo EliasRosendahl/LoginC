c4fc1b4b8f0eed23bc2e1a3e8a3a87c39db607b2
EliasRosendahl <jensen.eliasrosendahl@gmail.com>
2018-01-04 09:33:24 +0100

    added support for multiple users

--- login.c
+++ login.c
@@ -1,11 +1,22 @@
 #include <string.h>
+#include <stdio.h>
 
 //returns 0 if both username and password is correct
 //otherwise it returns 1
 
 int login(char name[], char password[]){
-    if(strncmp(name, "john\n", 32) == 0 && strncmp(password, "1234\n", 32) == 0){
-        return 0;
-    }
+
+    static char users[][2][32] = {
+            {"john\n", "1234\n"},
+            {"doe\n", "hunter2\n"},
+            {"rick\n", "roll\n"}
+    };
+
+    int i;
+    for(i = 0; i < 5; i++){
+        if(strncmp(users[i][0], name, 32) == 0 && strncmp(users[i][1], password, 32) == 0){
+            return 0;
+        }
+    };
     return 1;
 }
